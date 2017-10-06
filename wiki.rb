d1c89750e6a109d2ff0fda4abb60e285d4223bac
websitekay <websitekay@gmail.com>
2017-10-05 23:30:43 -0700

    params

--- wiki.rb
+++ wiki.rb
@@ -1,5 +1,15 @@
 require "sinatra"
 
+def page_content(title)
+  File.read("pages/#{title}.txt")
+rescue Errno::ENOENT
+  return nil
+end
+
 get "/" do
   erb :welcome
+end
+
+get "/:title" do
+  page_content(params[:title])
 end
