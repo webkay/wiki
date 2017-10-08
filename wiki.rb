24be5d70f91aa342717c3967cd376b63dcb9b06d
websitekay <websitekay@gmail.com>
2017-10-07 21:28:42 -0700

    Add "create a new page" link

--- wiki.rb
+++ wiki.rb
@@ -6,9 +6,20 @@ rescue Errno::ENOENT
   return nil
 end
 
+def save_content(title, content)
+  File.open("pages/#{title}.txt", 'w') do |file|
+    file.print(content)
+  end
+end
+
 get "/" do
   erb :welcome
 end
+
+get "/new" do
+  erb :new
+end
+
 
 get "/:title" do
   @title = params[:title]
