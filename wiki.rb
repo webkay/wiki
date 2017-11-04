9ab0147579632913f58882dab1dec1c523893741
websitekay <websitekay@gmail.com>
2017-11-04 12:55:35 -0700

    delete request

--- wiki.rb
+++ wiki.rb
@@ -13,6 +13,10 @@ def save_content(title, content)
   end
 end
 
+def delete_content(title)
+  File.delete("pages/#{title}.txt")
+end
+
 get "/" do
   erb :welcome
 end
@@ -41,4 +45,9 @@ end
 put "/:title" do
   save_content(params['title'], params['content'])
   redirect URI.escape "/#{params['title']}"
+end
+
+delete "/:title" do
+  delete_content(params[:title])
+  redirect "/"
 end
